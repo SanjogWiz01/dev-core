ac8e71626bb51625cf00fdf6fac3c3a2a9234890
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-04-01 19:45:13 +0545

    modified some files

--- ASSEMBLY LEVEL CODING/8085/07_add_16_bit.asm
+++ ASSEMBLY LEVEL CODING/8085/07_add_16_bit.asm
@@ -15,4 +15,4 @@ SHLD 3050H        ; Store result (LSB→3050H, MSB→3051H)
 STA 3052H         ; Store carry (00H or 01H)
 HLT
 ; This program adds two 16-bit numbers stored at memory locations 2050H-2051H and 2052H-2053H
-; The result is stored at memory locations 3050H-3051H and the carry (if any) is stored at 3052H
+; The result is stored at memory locations 3050H-3051H and the carry (if any) is stored at 3052H ; push
